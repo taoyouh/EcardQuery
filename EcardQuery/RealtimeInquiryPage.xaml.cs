4ad08b161f51d1c8c0ec02e3ab46d31062e2da9c
TaoyouH <zhaoquan2008@hotmail.com>
2015-12-28 16:37:47 +0800

    Revert "添加了查询页面内的过渡动画。"
    This reverts commit 959fab9672b14ed4cdadc5522dbd590eccac7388.

--- EcardQuery/RealtimeInquiryPage.xaml.cs
+++ EcardQuery/RealtimeInquiryPage.xaml.cs
@@ -49,9 +49,10 @@ namespace EcardQuery
 
                 //显示结果面板
                 isShowingData = true;
-                if (panelStates.CurrentState==controlState)
+                displayList.Visibility = Visibility.Visible;
+                if (ActualWidth < 600)
                 {
-                    VisualStateManager.GoToState(this, "dataState", true);
+                    controlPanel.Visibility = Visibility.Collapsed;
                 }
 
                 //查询数据
@@ -78,41 +79,31 @@ namespace EcardQuery
 
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
         {
-            if (e!= null)
-                if (e.NavigationMode==NavigationMode.Back && panelStates.CurrentState==dataState)
+            if (e != null)
+                if (isShowingData && controlPanel.Visibility == Visibility.Collapsed && e.NavigationMode == NavigationMode.Back)
                 {
                     e.Cancel = true;
-                    VisualStateManager.GoToState(this, "controlState", true);
+                    controlPanel.Visibility = Visibility.Visible;
+                    displayList.Visibility = Visibility.Collapsed;
                     isShowingData = false;
+                    return;
                 }
             base.OnNavigatingFrom(e);
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private void VisualStateGroup_CurrentStateChanging(object sender, VisualStateChangedEventArgs e)
         {
-            if (widthStates.CurrentState == wideState)
-                VisualStateManager.GoToState(this, "splitState", false);
-            else
+            if (e.NewState == wideState)
             {
-                if (isShowingData)
-                    VisualStateManager.GoToState(this, "dataState", false);
-                else
-                    VisualStateManager.GoToState(this, "controlState", false);
+                controlPanel.Visibility = Visibility.Visible;
+                displayList.Visibility = Visibility.Visible;
             }
-
-            base.OnNavigatedTo(e);
-        }
-
-        private void widthStates_CurrentStateChanged(object sender, VisualStateChangedEventArgs e)
-        {
-            if (widthStates.CurrentState == wideState)
-                VisualStateManager.GoToState(this, "splitState", false);
             else
             {
                 if (isShowingData)
-                    VisualStateManager.GoToState(this, "dataState", true);
+                    controlPanel.Visibility = Visibility.Collapsed;
                 else
-                    VisualStateManager.GoToState(this, "controlState", true);
+                    displayList.Visibility = Visibility.Collapsed;
             }
         }
     }
