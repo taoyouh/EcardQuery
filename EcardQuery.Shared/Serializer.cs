810d862b57089d38fa352c9a33b59e85ff77cd65
Taoyou H <zhaoquan2008@hotmail.com>
2016-12-14 20:24:53 +0800

    添加了将数据转换为csv字符串的函数。

---
+++ EcardQuery.Shared/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcardQuery
+{
+    static class Serializer
+    {
+        public static StringBuilder getCsv(IEnumerable<TransactionData> source)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("时间,交易类型,子系统名称,交易额,账户余额,卡内余额,次数");
+            foreach(var item in source)
+            {
+                builder.AppendFormat(
+                    "{0},{1},{2},{3},{4},{5},{6}\n",
+                    getCsvString( item.DateTime),
+                    getCsvString(item.TranscationType),
+                    getCsvString(item.SubSystem),
+                    getCsvString(item.Delta),
+                    getCsvString(item.AccountBalance),
+                    getCsvString(item.CardBalance),
+                    getCsvString(item.Id));
+            }
+            return builder;
+        }
+
+        static string getCsvString(object source)
+        {
+            return string.Format("\"{0}\"", source.ToString().Replace("\"", "\"\""));
+        }
+    }
+}
