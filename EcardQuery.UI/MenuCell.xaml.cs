df26cba249efb048079cf9a43d14d3c3e232b4fa
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-05-21 10:55:49 +0800

    优化UI

---
+++ EcardQuery.UI/MenuCell.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EcardQuery.UI
+{
+	public partial class MenuCell : ViewCell
+	{
+        public MenuCell()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create(nameof(Title), typeof(string), typeof(MenuCell), default(string));
+
+        public static readonly BindableProperty DetailProperty =
+            BindableProperty.Create(nameof(Detail), typeof(string), typeof(MenuCell), default(string));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string Detail
+        {
+            get => (string)GetValue(DetailProperty);
+            set => SetValue(DetailProperty, value);
+        }
+	}
+}
