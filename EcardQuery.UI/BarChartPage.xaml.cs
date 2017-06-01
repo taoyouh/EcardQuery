887df2877e886af421bf125ad5d287566a819bf5
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-06-01 22:28:14 +0800

    添加了柱状图页面。

---
+++ EcardQuery.UI/BarChartPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EcardQuery.UI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BarChartPage : ContentPage
+	{
+        UrlWebViewSource source;
+        IList<TransactionGroup> chartData;
+
+        public BarChartPage(IList<TransactionGroup> data)
+        {
+            InitializeComponent();
+
+            this.chartData = data;
+
+            source = new UrlWebViewSource()
+            {
+                Url = "Html/BarChart.html"
+            };
+            webView.Source = source;
+            webView.Navigated += WebView_Navigated;
+        }
+
+        private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            var dateAscending = chartData.Reverse();
+            var labels = from item in dateAscending select item.Key;
+            var values = from item in dateAscending select -item.TotalOutcome;
+            var labelsStr = Newtonsoft.Json.JsonConvert.SerializeObject(labels);
+            var valuesStr = Newtonsoft.Json.JsonConvert.SerializeObject(values);
+
+            webView.Eval($"labels = {labelsStr};");
+            webView.Eval($"values = {valuesStr};");
+            webView.Eval($"RefreshChart();");
+        }
+    }
+}
---
+++ EcardQuery.UI/Js/BarChart.js
@@ -0,0 +1,31 @@
+﻿console.log("BarChart.js loaded");
+function RefreshChart() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: labels,
+            datasets: [{
+                label: "消费金额",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: values,
+            }],
+
+        },
+
+        // Configuration options go here
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
